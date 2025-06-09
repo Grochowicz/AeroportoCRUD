98c354b266da482267041d3f9fb4c8d9cfe02bca
enzodear <enzo200325@gmail.com>
2025-06-09 04:47:28 -0300

    remove

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,39 @@
-import logo from './logo.svg';
 import React, {Component} from "react"
 import { Routes, Route, Link } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 
 import AddModelo from './components/AddModelo';
-import Tutorial from "./components/Tutorial";
+import Modelo from "./components/Modelo";
 import ModelosList from './components/ModelosList';
 
 function App() {
   return (
       <div>
         <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <a href="/tutorials" className="navbar-brand">
+          <a href="/modelos" className="navbar-brand">
             bezKoder
           </a>
           <div className="navbar-nav mr-auto">
             <li className="nav-item">
-              <Link to={"/tutorials"} className="nav-link">
+              <Link to={"/modelos"} className="nav-link">
                 Modelos
               </Link>
             </li>
             <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
+              <Link to={"/addModelo"} className="nav-link">
                 Adicionar Modelo
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link to={"/addEmpregado"} className="nav-link">
+                Adicionar Empregado
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to={"/empregado"} className="nav-link">
+                Empregados
               </Link>
             </li>
           </div>
@@ -32,9 +42,12 @@ function App() {
         <div className="container mt-3">
           <Routes>
             <Route path="/" element={<ModelosList/>} />
-            <Route path="/tutorials" element={<ModelosList/>} />
-            <Route path="/add" element={<AddModelo/>} />
-            <Route path="/tutorials/:id" element={<Tutorial/>} />
+            <Route path="/modelos" element={<ModelosList/>} />
+            <Route path="/addModelo" element={<AddModelo/>} />
+            <Route path="/modelos/:id" element={<Modelo/>} />
+
+            {/* <Route path="/empregado" element={<EmpregadosList/>} />
+            <Route path="/addEmpregado" element={<AddEmpregado/>} /> */}
           </Routes>
         </div>
       </div>
